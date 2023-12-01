4961fda89d0f726a49e83dbff345e94ffd57c801
jskrlec1 <josip.skrlecvk@gmail.com>
2023-12-01 16:53:53 +0100

    api2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import { Container, Button } from "react-bootstrap";
 
-function App() {
+const getRandomColor = () => {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+const App = () => {
+  const [imageData, setImageData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleButtonClick = () => {
+    axios
+      .get("https://api.unsplash.com/photos/random", {
+        params: {
+          query: "car", 
+          count: 1,
+          client_id: '2irblVvO1iJ9yYTUN3P4HcNGLmt_9DxJk-bOWL4kJ9A', 
+        },
+      })
+      .then(response => {
+        const photo = response.data[0];
+        const imageInfo = {
+          imageUrl: photo.urls.regular,
+          description: photo.alt_description || "Nema dostupnih informacija",
+        };
+        setImageData(imageInfo);
+        setError(null);
+      })
+      .catch(error => {
+        console.error("Greška prilikom dohvaćanja podataka:", error.message);
+        setError("Greška prilikom dohvaćanja informacija o slici");
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="mt-4">
+      <h1>Galerija slika</h1>
+      <Button onClick={handleButtonClick} className="btn btn-primary mt-2">
+        Dohvati sliku
+      </Button>
+      {error && <p className="text-danger mt-2">{error}</p>}
+      {imageData && (
+        <div className="mt-3">
+          <div
+            style={{
+              border: `1cm solid ${getRandomColor()}`,
+              overflow: "hidden",
+            }}
+          >
+            <img
+              src={imageData.imageUrl}
+              alt="Slika"
+              style={{ maxWidth: "100%", display: "block" }}
+            />
+          </div>
+          <p className="mt-2" style={{ color: "red", fontSize: "20px" }}>
+            Opis slike: {imageData.description}
+          </p>
+        </div>
+      )}
+    </Container>
   );
-}
+};
 
 export default App;
+
